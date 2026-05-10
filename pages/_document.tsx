6ad6a876afd0fa2ebc4a20e1c90361051262d500
Rubén Santaella <rubensdlt@gmail.com>
2026-05-10 17:17:48 +0200

    Add Pirsch analytics tracking script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          src="https://api.pirsch.io/pa.js"
+          id="pianjs"
+          data-code="yZblf0fGEfpnhCyGrrwAD2bNW4p29fa4"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
